6c88cfd1032168b61822c2434f6a94d03eca88ef
Corina Talavera <corina.talavera@gmail.com>
2018-11-15 00:55:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,11 +4,6 @@ class Dog
   def initialize(name)
     @name = name
     @@all << self
-  end  
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
-    end
   end
   def self.clear_all
     @@all.clear
