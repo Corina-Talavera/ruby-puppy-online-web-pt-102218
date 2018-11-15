344e9703137e1d31f7f0864de01fa8e247428234
Corina Talavera <corina.talavera@gmail.com>
2018-11-15 00:48:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,17 +8,14 @@ class Dog
     @name = name
     @@all << self
   end
-
   def self.all
     @@all.each do |dog|
       puts dog.name
     end
   end
-
   def self.clear_all
     @@all.clear
   end
-
 end
 
 Dog.new("Pluto")
