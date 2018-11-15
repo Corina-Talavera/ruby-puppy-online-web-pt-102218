0a84b25fa8d3058d113780ea2f9ddc6f6998dece
Corina Talavera <corina.talavera@gmail.com>
2018-11-15 00:45:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,22 @@
-class Dog 
-  Dog.new("Pluto")
-  Dog.new("Fido")
-  Dog.new("Maddy")
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+end
